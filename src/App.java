c6a7a248c6b9fb10cb67c82dea365f751c729ac9
Rebecca <No>
2024-09-26 08:38:55 -0300

    madlib done :D

--- src/App.java
+++ src/App.java
@@ -19,7 +19,26 @@ public class App {
      * Document what this method does here!
      */
     public String getUserName () {
-        System.out.println("Hii, enter your name.");
+
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠖⠚⠛⠓⣦⠀⠀");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢸⡆⠀");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⢸⡇⠀");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠘⡇⠀⠀⠀⠀⢸⡇⠀");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⡇⠀");
+        System.out.println ("⠀⠀⠀⢀⣴⣶⣦⣄⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⠋⠛⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⣷⠀");
+        System.out.println ("⠀⠀⣴⣿⣿⣿⣿⣿⣷⣄⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⢸⡀");
+        System.out.println ("⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡧⠀⠀⠀⢰⣶⣶⣤⣼⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⣀⣀⣀⣀⣠⡾⠁");
+        System.out.println ("⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⡿⠃⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⢬⣉⠉⣉⣀⡀⠀");
+        System.out.println ("⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠀⠀⠙⣿⣿⣿⠟⠁⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠋⠀⠀⠉⠉⠉⠀⠙⡆");
+        System.out.println ("⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⢀⣼⠇");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⣠⠞⠁⠀");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠿⢿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠋⠁⠀⠀⠀");
+        System.out.println ("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+        System.out.println (" ");
+
+
+
+        System.out.println("Please enter your name.");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         return name; 
@@ -31,12 +50,49 @@ public class App {
     }
 
      public void printInstructions() {
-       System.out.println("Answer these questions to get a funny story!!");
+       System.out.println("Put in the following information to get a funny story!!");
     
     }
     public void doMadlib() {
 
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println ("Number:");
+        String numberOne = input.nextLine();
+        
+        System.out.println ("Adjective:");
+        String adjectiveOne = input.nextLine();
+
+        System.out.println ("Boy Name:");
+        String boyNameOne = input.nextLine();
+
+        System.out.println ("Food:");
+        String foodOne = input.nextLine();
+
+        System.out.println ("Adjective:");
+        String adjectiveTwo = input.nextLine();
+
+        System.out.println ("Noun:");
+        String nounOne = input.nextLine();
+
+        System.out.println ("Adjective:");
+        String adjectiveThree = input.nextLine();
+
+        System.out.println ("Animal:");
+        String animalOne = input.nextLine();
+
+        System.out.println ("Past tense verb:");
+        String pastTenseVerbOne = input.nextLine();
+
+        System.out.println ("Adjective:");
+        String adjectiveFour = input.nextLine();
+
+        System.out.println("Once, there was a group of " + numberOne + " teens, they were so " + adjectiveOne + ". One of them was named " + boyNameOne + ", he liked to eat " + foodOne + ". One " + adjectiveTwo + " morning, the " + numberOne + " teens decided to go explore a haunted " + nounOne + ". But, there night was cut short when they were attacked by a/an" + adjectiveThree + " deadbeb " + animalOne + ". They " + pastTenseVerbOne + " and got away from the ghost. " + boyNameOne + " then went to his " + adjectiveFour + " house and enjoyed a plate of " + foodOne + ".");
+
     }
+
+    
     
     // add and implement the other methods you need in this area below!
     
